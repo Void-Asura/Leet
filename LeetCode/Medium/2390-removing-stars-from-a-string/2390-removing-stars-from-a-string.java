3b0f725538d9f704fedc2394b75c57b8835ffde6
Mahesh Sharma <ms23082003@gmail.com>
2025-05-18 22:20:08 +0530

    69 ms (54.81%) 45.6 MB (88.41%) 05-18-2025 2390-removing-stars-from-a-string Medium .java

--- LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ LeetCode/Medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -2,32 +2,35 @@ class Solution
 {
     public String removeStars(String s) 
     {
-        // StringBuffer sbr=new StringBuffer(s);
-        // for(int i=0;i<sbr.length();i++)
-        // {
-        //     if(sbr.charAt(i)=='*')
-        //     {
-        //         sbr.delete(i-1,i+1);
-        //     }
-        // }
-        // return sbr.toString();
-        Stack<Character> st=new Stack<>();
+        StringBuffer sbr=new StringBuffer();
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='*')
             {
-                if(!st.isEmpty())
-                st.pop();
+                if(sbr.length()>0)
+                    sbr.delete(sbr.length()-1,sbr.length());
             }
             else
-            st.push(s.charAt(i));
+            sbr.append(s.charAt(i));
         }
-        StringBuffer sbr=new StringBuffer();
-        while(!st.isEmpty())
-        {
-            sbr.append(st.pop());
-        }
-        return sbr.reverse().toString();
+        return sbr.toString();
+        // Stack<Character> st=new Stack<>();
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s.charAt(i)=='*')
+        //     {
+        //         if(!st.isEmpty())
+        //         st.pop();
+        //     }
+        //     else
+        //     st.push(s.charAt(i));
+        // }
+        // StringBuffer sbr=new StringBuffer();
+        // while(!st.isEmpty())
+        // {
+        //     sbr.append(st.pop());
+        // }
+        // return sbr.reverse().toString();
 
     }
 }
