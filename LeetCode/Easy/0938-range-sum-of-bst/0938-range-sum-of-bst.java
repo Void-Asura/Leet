fbabc241456b39d66b7ec6431509cd67be84769a
Mahesh Sharma <ms23082003@gmail.com>
2025-05-25 23:31:44 +0530

    0 ms (100%) 51.4 MB (36.77%) 05-25-2025 0938-range-sum-of-bst Easy .java

--- LeetCode/Easy/0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ LeetCode/Easy/0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -17,15 +17,26 @@ class Solution
 {
     public int rangeSumBST(TreeNode root, int low, int high) 
     {
-        if(root==null)
-        return 0;
+        // if(root==null)
+        // return 0;
 
-        int S=0;
-        if(root.val>=low&&root.val<=high)
-        S+=root.val;
-        S+=rangeSumBST(root.left,low,high);
-        S+=rangeSumBST(root.right,low,high);
+        // int S=0;
+        // if(root.val>=low&&root.val<=high)
+        // S+=root.val;
+        // S+=rangeSumBST(root.left,low,high);
+        // S+=rangeSumBST(root.right,low,high);
 
-        return S;
+        // return S;
+
+        if (root == null) return 0;
+        
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high); // Skip left subtree
+        } else if (root.val > high) {
+            return rangeSumBST(root.left, low, high); // Skip right subtree
+        } else {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        }
+
     }
 }
