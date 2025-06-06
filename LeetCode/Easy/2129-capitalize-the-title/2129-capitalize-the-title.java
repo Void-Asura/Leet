f5b7481661d1ba087a9aad7d540dcd6102ed1e49
Mahesh Sharma <ms23082003@gmail.com>
2025-06-06 23:00:25 +0530

    3 ms (79.58%) 42.6 MB (57.33%) 06-06-2025 2129-capitalize-the-title Easy .java

---
+++ LeetCode/Easy/2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public String capitalizeTitle(String title) 
+    {
+        String[] words = title.split(" ");
+        StringBuilder sb = new StringBuilder();
+        
+        for (String word : words) {
+            if (word.length() <= 2) {
+                sb.append(word.toLowerCase());
+            } else {
+                sb.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1).toLowerCase());
+            }
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
