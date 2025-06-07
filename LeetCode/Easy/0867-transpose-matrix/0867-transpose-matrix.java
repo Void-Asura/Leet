ebb2a64239322130c085c6f6ffc4e8aa572b2e76
Mahesh Sharma <ms23082003@gmail.com>
2025-06-07 21:47:13 +0530

    1 ms (24.08%) 45.2 MB (21.63%) 06-07-2025 0867-transpose-matrix Easy .java

---
+++ LeetCode/Easy/0867-transpose-matrix/0867-transpose-matrix.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int[][] transpose(int[][] matrix) 
+    {
+        int tra[][]=new int[matrix[0].length][matrix.length];
+        for(int i=0;i<matrix.length;i++)
+        {
+            for(int j=0;j<matrix[0].length;j++)
+            {
+                tra[j][i]=matrix[i][j];
+            }
+        }
+        return tra;
+    }
+}
