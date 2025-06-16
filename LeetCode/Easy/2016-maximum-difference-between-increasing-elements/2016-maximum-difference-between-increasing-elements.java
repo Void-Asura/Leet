bf2642aefe9a4c70575b27b89e1f04f8d08431ab
Mahesh Sharma <ms23082003@gmail.com>
2025-06-16 20:20:06 +0530

    0 ms (100%) 41.9 MB (73.85%) 06-16-2025 2016-maximum-difference-between-increasing-elements Easy .java

--- LeetCode/Easy/2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
+++ LeetCode/Easy/2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.java
@@ -2,14 +2,21 @@ class Solution
 {
     public int maximumDifference(int[] nums) 
     {
+        int minn=nums[0];
         int diff=-1;
-        for(int i=0;i<nums.length-1;i++)
+        for(int i=1;i<nums.length;i++)
         {
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]<nums[j])
-                diff=Math.max(diff,nums[j]-nums[i]);
+            // for(int j=i+1;j<nums.length;j++)
+            // {
+            //     if(nums[i]<nums[j])
+            //     diff=Math.max(diff,nums[j]-nums[i]);
+            // }
+            if (nums[i] > minn) {
+                diff = Math.max(diff, nums[i] - minn);
+            } else {
+                minn = nums[i];
             }
+
         }
         return diff;
     }
