64f920b284ebe0f0a05dc7bc0f6123cd8846a695
Mahesh Sharma <ms23082003@gmail.com>
2025-06-25 23:06:41 +0530

    0 ms (100%) 41.9 MB (29.8%) 06-25-2025 1832-check-if-the-sentence-is-pangram Easy .java

--- LeetCode/Easy/1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ LeetCode/Easy/1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -2,11 +2,18 @@ class Solution
 {
     public boolean checkIfPangram(String sentence) 
     {
-        Set<Character> s=new HashSet<>();
-        for(int i=0;i<sentence.length();i++)
+        // Set<Character> s=new HashSet<>();
+        // for(int i=0;i<sentence.length();i++)
+        // {
+        //     s.add(sentence.charAt(i));
+        // }
+        // return s.size()==26?true:false;
+
+        for(char i='a';i<='z';i++)
         {
-            s.add(sentence.charAt(i));
+            if(sentence.indexOf(i)<0)
+            return false;
         }
-        return s.size()==26?true:false;
+        return true;
     }
 }
