db2f5c363cf99cb08a5a1b6bfacf231a2eeb3e7d
Mahesh Sharma <ms23082003@gmail.com>
2025-06-26 23:21:37 +0530

    4 ms (55.55%) 45.1 MB (29.03%) 06-26-2025 0125-valid-palindrome Easy .java

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,19 +3,26 @@ class Solution
     public boolean isPalindrome(String s) 
     {
         s=s.toLowerCase();
-        String str="";
-        for(int i=0;i<s.length();i++)
+        // String str="";
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(Character.isLetterOrDigit(s.charAt(i)))
+        //     str+=s.charAt(i);
+        // }
+        // // StringBuffer sbr=new StringBuffer(str);
+        // // return str.equals(sbr.reverse().toString());
+        // String rev="";
+        // for(int i=str.length()-1;i>=0;i--)
+        // {
+        //     rev+=str.charAt(i);
+        // }
+        // return str.equals(rev);
+        StringBuilder sbr=new StringBuilder();
+        for(char ch:s.toCharArray())
         {
-            if(Character.isLetterOrDigit(s.charAt(i)))
-            str+=s.charAt(i);
+            if(Character.isLetterOrDigit(ch))
+            sbr.append(ch);
         }
-        // StringBuffer sbr=new StringBuffer(str);
-        // return str.equals(sbr.reverse().toString());
-        String rev="";
-        for(int i=str.length()-1;i>=0;i--)
-        {
-            rev+=str.charAt(i);
-        }
-        return str.equals(rev);
+        return sbr.toString().equals(sbr.reverse().toString());
     }
 }
