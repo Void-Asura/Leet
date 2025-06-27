e212bb5a3c36098449f71ee5fad3343ef8916040
Mahesh Sharma <ms23082003@gmail.com>
2025-06-27 22:26:20 +0530

    0 ms (100%) 41.6 MB (52.64%) 06-27-2025 0796-rotate-string Easy .java

--- LeetCode/Easy/0796-rotate-string/0796-rotate-string.java
+++ LeetCode/Easy/0796-rotate-string/0796-rotate-string.java
@@ -2,15 +2,19 @@ class Solution
 {
     public boolean rotateString(String s, String goal) 
     {        
-        int n=1;
-        while(n<=s.length())
-        {
-            char ch=s.charAt(0);
-            s=s.substring(1,s.length())+ch;
-            if(s.equals(goal))
-            return true;
-            n++;
-        }
+        // int n=1;
+        // while(n<=s.length())
+        // {
+        //     char ch=s.charAt(0);
+        //     s=s.substring(1,s.length())+ch;
+        //     if(s.equals(goal))
+        //     return true;
+        //     n++;
+        // }
+        // return false;
+
+        if((s+s).contains(goal) && s.length()==goal.length())
+        return true;
         return false;
     }
 }
