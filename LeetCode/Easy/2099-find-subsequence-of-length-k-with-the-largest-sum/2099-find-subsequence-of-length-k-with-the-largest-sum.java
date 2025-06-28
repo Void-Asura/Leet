50729da0ecd737d207a1d637b8180eba8617bfec
Mahesh Sharma <ms23082003@gmail.com>
2025-06-28 22:45:32 +0530

    12 ms (11.42%) 43.7 MB (99.08%) 06-28-2025 2099-find-subsequence-of-length-k-with-the-largest-sum Easy .java

--- LeetCode/Easy/2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
+++ LeetCode/Easy/2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.java
@@ -2,36 +2,61 @@ class Solution
 {
     public int[] maxSubsequence(int[] nums, int k) 
     {
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        int i=0;
-        for(;i<k;i++)
-        {
-            pq.add(nums[i]);
-        }
+        // PriorityQueue<Integer> pq=new PriorityQueue<>();
+        // int i=0;
+        // for(;i<k;i++)
+        // {
+        //     pq.add(nums[i]);
+        // }
 
-        while(i<nums.length)
+        // while(i<nums.length)
+        // {
+        //     if(nums[i]>pq.peek())
+        //     {
+        //         pq.remove();
+        //         pq.add(nums[i]);
+        //     }
+        //     i++;
+        // }
+        // int arr[]=new int[k];
+        // int k1=0;
+        // for(int j=0;j<nums.length;j++)
+        // {
+        //     if(pq.contains(nums[j]))
+        //     {
+        //         arr[k1]=nums[j];
+        //         k1++;
+        //         pq.remove(nums[j]);
+        //     }
+        //    if(k1==k)
+        //     break;
+        // }
+        // return arr;
+        if(k==nums.length)
+        return nums;
+
+        int arr[]=nums.clone();
+        Arrays.sort(arr);
+        int res[]=new int[k];
+        List<Integer> ls=new ArrayList<>();
+        int i=nums.length-1;
+        while(k>0)
         {
-            if(nums[i]>pq.peek())
-            {
-                pq.remove();
-                pq.add(nums[i]);
-            }
-            i++;
+            ls.add(arr[i]);
+            i--;
+            k--;
         }
-        int arr[]=new int[k];
-        int k1=0;
+        int temp=0;
         for(int j=0;j<nums.length;j++)
         {
-            if(pq.contains(nums[j]))
+            if(ls.contains(nums[j]))
             {
-                arr[k1]=nums[j];
-                k1++;
-                pq.remove(nums[j]);
+                res[temp]=nums[j];
+                temp++;
+                ls.remove((Integer)nums[j]);
             }
-           if(k1==k)
-            break;
         }
-        return arr;
+        return res;
 
     }
 }
