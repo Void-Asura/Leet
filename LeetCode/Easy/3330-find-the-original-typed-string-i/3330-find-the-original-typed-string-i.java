4a560f032152baf875978b56c210db14d57403ee
Mahesh Sharma <ms23082003@gmail.com>
2025-07-01 21:47:45 +0530

    1 ms (87.19%) 42.2 MB (61.16%) 07-01-2025 3330-find-the-original-typed-string-i Easy .java

--- LeetCode/Easy/3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
+++ LeetCode/Easy/3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
@@ -4,23 +4,36 @@ class Solution
     
     public int possibleStringCount(String word) 
     {
-        if(word.length()>1)
+    //     if(word.length()>1)
+    // {
+    //     StringBuilder sbr=new StringBuilder(word);
+    //     int i=0,tot=1;
+    //     while(i<sbr.length()-1)
+    //     {
+    //         if(sbr.charAt(i)==sbr.charAt(i+1))
+    //         {
+    //             tot++;
+    //             sbr.deleteCharAt(i+1);
+    //         }
+    //         else
+    //         i++;
+    //     }
+    //     return tot;
+    // }
+    // else
+    // return 1;
+
+    int ans=1;
+    char c=word.charAt(0);
+    for(int i=1;i<word.length();i++)
     {
-        StringBuilder sbr=new StringBuilder(word);
-        int i=0,tot=1;
-        while(i<sbr.length()-1)
+        char temp=word.charAt(i);
+        if(temp==c)
         {
-            if(sbr.charAt(i)==sbr.charAt(i+1))
-            {
-                tot++;
-                sbr.deleteCharAt(i+1);
-            }
-            else
-            i++;
+            ans++;
         }
-        return tot;
+        else c=temp;
     }
-    else
-    return 1;
+    return ans;
     }
 }
