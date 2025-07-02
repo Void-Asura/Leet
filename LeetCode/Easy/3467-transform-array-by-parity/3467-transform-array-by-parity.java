ebb2f0df6980f2410a926051047d586bacbf335c
Mahesh Sharma <ms23082003@gmail.com>
2025-07-03 00:52:51 +0530

    1 ms (100%) 45.3 MB (13.38%) 07-03-2025 3467-transform-array-by-parity Easy .java

--- LeetCode/Easy/3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ LeetCode/Easy/3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,52 +1,49 @@
-class Solution 
-{
-    public int[] transformArray(int[] nums) 
-    {
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]%2==0)
-            nums[i]=0;
+class Solution {
+    public int[] transformArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0)
+                nums[i] = 0;
             else
-            nums[i]=1;
+                nums[i] = 1;
         }
-        quicksort(nums,0,nums.length-1);
+        quicksort(nums, 0, nums.length - 1);
         return nums;
 
     }
-    public static void quicksort(int arr[], int lowIndex, int highIndex)
-    {
-        if(lowIndex >= highIndex)
-        return;
 
-        int pivot=arr[highIndex];
+    public static void quicksort(int arr[], int lowIndex, int highIndex) {
+        if (lowIndex >= highIndex)
+            return;
 
-        int leftPointer=lowIndex;
-        int rightPointer=highIndex;
+        int pivot = arr[highIndex];
 
-        while(leftPointer < rightPointer)
-        {
-            while(arr[leftPointer]<=pivot && leftPointer < rightPointer)
-            {
+        int leftPointer = lowIndex;
+        int rightPointer = highIndex;
+
+        while (leftPointer < rightPointer) {
+            while (arr[leftPointer] <= pivot && leftPointer < rightPointer) {
                 leftPointer++;
             }
-            while(arr[rightPointer]>=pivot && leftPointer<rightPointer)
-            {
+            while (arr[rightPointer] >= pivot && leftPointer < rightPointer) {
                 rightPointer--;
             }
-            swap(arr,leftPointer,rightPointer);
+            swap(arr, leftPointer, rightPointer);
 
         }
-        swap(arr,leftPointer,highIndex);
-        
-        quicksort(arr,lowIndex,leftPointer-1);
-        quicksort(arr,leftPointer+1,highIndex);
+        if (arr[leftPointer] > arr[highIndex]) {
+            swap(arr, leftPointer, highIndex);
+        } else {
+            leftPointer = highIndex;
+        }
+
+        quicksort(arr, lowIndex, leftPointer - 1);
+        quicksort(arr, leftPointer + 1, highIndex);
 
     }
 
-    public static void swap(int []array,int index1,int index2)
-    {
-        int temp=array[index1];
-        array[index1]=array[index2];
-        array[index2]=temp;
+    public static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
