3ffb8137641cf541750ba6f049773bb543a587ba
Mahesh Sharma <ms23082003@gmail.com>
2025-07-02 23:46:44 +0530

    1 ms (100%) 45.1 MB (23.91%) 07-02-2025 3467-transform-array-by-parity Easy .java

---
+++ LeetCode/Easy/3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -0,0 +1,52 @@
+class Solution 
+{
+    public int[] transformArray(int[] nums) 
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]%2==0)
+            nums[i]=0;
+            else
+            nums[i]=1;
+        }
+        quicksort(nums,0,nums.length-1);
+        return nums;
+
+    }
+    public static void quicksort(int arr[], int lowIndex, int highIndex)
+    {
+        if(lowIndex >= highIndex)
+        return;
+
+        int pivot=arr[highIndex];
+
+        int leftPointer=lowIndex;
+        int rightPointer=highIndex;
+
+        while(leftPointer < rightPointer)
+        {
+            while(arr[leftPointer]<=pivot && leftPointer < rightPointer)
+            {
+                leftPointer++;
+            }
+            while(arr[rightPointer]>=pivot && leftPointer<rightPointer)
+            {
+                rightPointer--;
+            }
+            swap(arr,leftPointer,rightPointer);
+
+        }
+        swap(arr,leftPointer,highIndex);
+        
+        quicksort(arr,lowIndex,leftPointer-1);
+        quicksort(arr,leftPointer+1,highIndex);
+
+    }
+
+    public static void swap(int []array,int index1,int index2)
+    {
+        int temp=array[index1];
+        array[index1]=array[index2];
+        array[index2]=temp;
+    }
+}
