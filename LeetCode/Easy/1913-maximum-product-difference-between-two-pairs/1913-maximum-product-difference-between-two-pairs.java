ed58459d6b2629515d862a462e409d177dffb702
Mahesh Sharma <ms23082003@gmail.com>
2025-07-17 23:41:08 +0530

    7 ms (63.35%) 45.1 MB (52.34%) 07-17-2025 1913-maximum-product-difference-between-two-pairs Easy .java

---
+++ LeetCode/Easy/1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -0,0 +1,49 @@
+class Solution 
+{
+    public int maxProductDifference(int[] nums) 
+    {
+        int l=nums.length;
+        quicksort(nums, 0, l-1);
+        return (nums[l-1]*nums[l-2])-(nums[0]*nums[1]);
+    }
+    public static void quicksort(int arr[],int lowIndex, int highIndex)
+    {
+        if(lowIndex>=highIndex)
+        return;
+
+        int pivot=arr[highIndex];
+        int leftPointer=lowIndex;
+        int rightPointer=highIndex-1;
+
+        while(leftPointer<rightPointer)
+        {
+            while(leftPointer < rightPointer && arr[leftPointer] <= pivot)
+            {
+                leftPointer++;
+            }
+            while(leftPointer < rightPointer && arr[rightPointer] >= pivot)
+            {
+                rightPointer--;
+            }
+            swap(arr,leftPointer,rightPointer);
+        }
+
+        if(arr[leftPointer]>arr[highIndex])
+        {
+            swap(arr,leftPointer,highIndex);
+        }
+        else
+        {
+            leftPointer=highIndex;
+        }
+
+        quicksort(arr, lowIndex, leftPointer-1);
+        quicksort(arr, leftPointer+1, highIndex);
+    }
+    public static void swap(int [] arr, int index1, int index2)
+    {
+        int temp=arr[index1];
+        arr[index1]=arr[index2];
+        arr[index2]=temp;
+    }
+}
