f674eb9d10dbbe631ce2fc48dde03658edd387ef
Mahesh Sharma <ms23082003@gmail.com>
2025-07-21 21:46:30 +0530

    1699 ms (5.05%) 45.8 MB (60.35%) 07-21-2025 1957-delete-characters-to-make-fancy-string Easy .java

---
+++ LeetCode/Easy/1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.java
@@ -0,0 +1,58 @@
+class Solution 
+{
+    public String makeFancyString(String s) 
+    {
+        // int len=1;
+        // char arr[]=s.toCharArray();        
+        // char ch=arr[0];
+        // for(int i=1;i<arr.length;i++)
+        // {
+        //     if(arr[i]==ch)
+        //     {
+        //         len++;
+        //         if(len>2)
+        //         {
+        //             arr[i]='-';
+        //         }
+        //     }
+        //     else
+        //     {
+        //         ch=arr[i];
+        //         len=1;
+        //     }
+        // }
+        // String str="";
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     if(arr[i]!='-')
+        //     {
+        //         str+=String.valueOf(arr[i]);
+        //     }
+        // }
+        // return str;
+
+        int c=1,i=0;
+        StringBuilder sbr=new StringBuilder(s);
+        while(i<sbr.length())
+        {
+            if(i>0 && sbr.charAt(i)==sbr.charAt(i-1))
+            {
+                if(c==2)
+                {
+                    sbr.deleteCharAt(i);
+                }
+                else
+                {
+                    c++;
+                    i++;
+                }
+            }
+            else
+            {
+                c=1;
+                i++;
+            }
+        }
+        return sbr.toString();
+    }
+}
