afd9c1fb8e0acba5965a38114fa09dd61ae0537b
Mahesh Sharma <ms23082003@gmail.com>
2025-08-31 21:34:25 +0530

    3 ms (12.03%) 44.4 MB (72.7%) 08-31-2025 3248-snake-in-matrix Easy .java

---
+++ LeetCode/Easy/3248-snake-in-matrix/3248-snake-in-matrix.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public int finalPositionOfSnake(int n, List<String> commands) 
+    {
+        int i=0,j=0;
+        for(String s:commands)
+        {
+            if(s.equals("UP"))
+            {
+                --i;
+            }
+            else if(s.equals("DOWN"))
+            {
+                ++i;
+            }
+            else if(s.equals("LEFT"))
+            {
+                --j;
+            }
+            else
+            {
+                ++j;
+            }
+        }
+        return (i*n)+j;
+    }
+}
