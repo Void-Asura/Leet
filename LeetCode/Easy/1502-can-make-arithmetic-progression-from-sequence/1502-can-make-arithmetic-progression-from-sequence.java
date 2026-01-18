c5c90a367d718250e836603675fdc328ff2de7b5
Mahesh Sharma <ms23082003@gmail.com>
2026-01-18 23:31:25 +0530

    5 ms (83.68%) 44.3 MB (29.42%) 01-18-2026 1502-can-make-arithmetic-progression-from-sequence Easy .java

--- LeetCode/Easy/1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ LeetCode/Easy/1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -2,16 +2,13 @@ class Solution
 {
     public boolean canMakeArithmeticProgression(int[] arr) 
     {
-         Arrays.sort(arr);
-
-        int diff = arr[1] - arr[0];
-
-        for (int i = 2; i < arr.length; i++) 
+        Arrays.sort(arr);
+        int diff=arr[1]-arr[0];
+        for(int i=1;i<=arr.length-2;i++)
         {
-            if (arr[i] - arr[i - 1] != diff)
-                return false;
+            if(diff!=(arr[i+1]-arr[i]))
+            return false;
         }
-
         return true;
     }
 }
