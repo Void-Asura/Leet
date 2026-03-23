b12366fc17fa03357f0e58286b37139a3ed2cc48
Mahesh Sharma <ms23082003@gmail.com>
2026-03-24 01:51:40 +0530

    1 ms (67.54%) 42.3 MB (43.88%) 03-24-2026 0670-maximum-swap Medium .java

---
+++ LeetCode/Medium/0670-maximum-swap/0670-maximum-swap.java
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public int maximumSwap(int num) 
+    {
+        char[] digits = Integer.toString(num).toCharArray();
+
+        int[] last = new int[10];
+
+        // store last occurrence of each digit
+        for (int i = 0; i < digits.length; i++) 
+        {
+            last[digits[i] - '0'] = i;
+        }
+
+        // try to swap
+        for (int i = 0; i < digits.length; i++) 
+        {
+            for (int d = 9; d > digits[i] - '0'; d--) 
+            {
+                if (last[d] > i) 
+                {
+                    char temp = digits[i];
+                    digits[i] = digits[last[d]];
+                    digits[last[d]] = temp;
+
+                    return Integer.parseInt(new String(digits));
+                }
+            }
+        }
+
+        return num;
+    }
+}
