ffb28ef7df6a6c25325dff289fb3d111cafbc79c
Mahesh Sharma <ms23082003@gmail.com>
2026-04-12 01:09:18 +0530

    0 ms (100%) 42.1 MB (68.59%) 04-12-2026 0365-water-and-jug-problem Medium .java

---
+++ LeetCode/Medium/0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public boolean canMeasureWater(int x, int y, int target) 
+    {
+
+        if (target > x + y) return false;
+
+        return target % gcd(x, y) == 0;
+    }
+
+    private int gcd(int a, int b) 
+    {
+        while (b != 0) 
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
