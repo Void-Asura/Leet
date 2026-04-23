483e5e09c448c259639b833839ac738377a0dcb7
Mahesh Sharma <ms23082003@gmail.com>
2026-04-24 00:04:50 +0530

    1 ms (87.44%) 42.5 MB (69.68%) 04-24-2026 3870-count-commas-in-range Easy .java

---
+++ LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,21 @@
+class Solution 
+{
+    public int countCommas(int n) 
+    {
+        int commas = 0;
+        long start = 1000;
+        int commaCount = 1;
+
+        while (start <= n) 
+        {
+            long end = Math.min(n, start * 1000 - 1);
+
+            commas += (end - start + 1) * commaCount;
+
+            start *= 1000;
+            commaCount++;
+        }
+
+        return commas;
+    }
+}
